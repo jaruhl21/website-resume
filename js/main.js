80a5ff766f5b5ae1deff871358f284910588eb1b
jaruhl21 <jruhl@microtipsusa.com>
2018-02-27 17:58:01 -0500

    added progress bars and working on show animation

--- js/main.js
+++ js/main.js
@@ -1,9 +1,29 @@
-var heightMinusSeventy = $(window).height() - 70;
+var windowHeight = $(window).height();
 
 $(window).scroll(function() {
-	if ($(document).scrollTop() > heightMinusSeventy) {
+	if ($(document).scrollTop() > 0) {
 		$('.nav').addClass('shrink');
 	} else {
 		$('.nav').removeClass('shrink');
 	}
 });
+
+$(document).ready(function() {
+	progressBarPosition = $('.progressBars').position();
+	var vertProgressBarPosition = progressBarPosition.top;
+	alert(vertProgressBarPosition);
+	var showProgressBars = vertProgressBarPosition - windowHeight + 300;
+	alert(showProgressBars);
+
+	$(window).scroll(function() {
+		if ($(document).scrollTop() > showProgressBars) {
+			$('.progressBars').show();
+		} else {
+			$('.progressBars').hide();
+		}
+	});
+});
+
+// $('.indexSectionStudent').waypoint(function() {
+// 	alert('The Student Section is on the screen')
+// });
