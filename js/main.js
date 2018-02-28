66548c084aa576110939498c4e4b6fbe0644c565
jaruhl21 <jruhl@microtipsusa.com>
2018-02-28 11:52:36 -0500

    working on a parallax effect. Not getting the same as the squarespace site. Need to accept it or keep experimenting.

--- js/main.js
+++ js/main.js
@@ -9,19 +9,53 @@ $(window).scroll(function() {
 });
 
 $(document).ready(function() {
-	progressBarPosition = $('.progressBars').position();
+	var progressBarPosition = $('.progressBars').position();
 	var vertProgressBarPosition = progressBarPosition.top;
-	alert(vertProgressBarPosition);
-	var showProgressBars = vertProgressBarPosition - windowHeight + 300;
-	alert(showProgressBars);
+	// alert(vertProgressBarPosition);
+	var showProgressBars = vertProgressBarPosition - windowHeight + 200;
+	// alert(showProgressBars);
+	var progressBarHeight = $('.progressBarRowOne').height();
 
 	$(window).scroll(function() {
 		if ($(document).scrollTop() > showProgressBars) {
-			$('.progressBars').show();
+			$('.progressBarRowOne').show();
 		} else {
-			$('.progressBars').hide();
+			$('.progressBarRowOne').hide();
+		}
+		if ($(document).scrollTop() > showProgressBars + progressBarHeight*1) {
+			$('.progressBarRowTwo').show();
+		} else {
+			$('.progressBarRowTwo').hide();
+		}
+		if ($(document).scrollTop() > showProgressBars + progressBarHeight*2) {
+			$('.progressBarRowThree').show();
+		} else {
+			$('.progressBarRowThree').hide();
+		}
+		if ($(document).scrollTop() > showProgressBars + progressBarHeight*3) {
+			$('.progressBarRowFour').show();
+		} else {
+			$('.progressBarRowFour').hide();
+		}
+		if ($(document).scrollTop() > showProgressBars + progressBarHeight*4) {
+			$('.progressBarRowFive').show();
+		} else {
+			$('.progressBarRowFive').hide();
 		}
 	});
+});
+
+$(window).scroll(function() {
+	var scrollTopVal = $(this).scrollTop();
+	var scrollTopValPercentage = scrollTopVal/windowHeight*20 + 50;
+	var microtipsHeight = $('.indexSectionMicrotips').position();
+	var microtipsheightVal = microtipsHeight.top;
+	var x = scrollTopVal/microtipsheightVal*20 + 30;
+	console.log("s: " + scrollTopVal + " m: " + microtipsheightVal + " x: " + x);
+	console.log(scrollTopValPercentage);
+
+	$('.indexSectionOne').css('background-position', '50% ' + scrollTopValPercentage + '%');
+	$('.indexSectionMicrotips').css('background-position', '50% ' + x + '%');
 });
 
 // $('.indexSectionStudent').waypoint(function() {
